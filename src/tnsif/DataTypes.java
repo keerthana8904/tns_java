daa846ed1c83e799e91ce819fefef3f6933cb09b
Keerthana G <keerthanag3292@gmail.com>
2026-02-21 19:56:07 +0530

    Added DataTypes program

---
+++ src/tnsif/DataTypes.java
@@ -0,0 +1,21 @@
+package tnsif;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        double b = 20.5;
+        char c = 'K';
+        boolean d = true;
+        float e = 5.5f;
+        long f = 100000L;
+
+        System.out.println("Integer: " + a);
+        System.out.println("Double: " + b);
+        System.out.println("Character: " + c);
+        System.out.println("Boolean: " + d);
+        System.out.println("Float: " + e);
+        System.out.println("Long: " + f);
+    }
+}
